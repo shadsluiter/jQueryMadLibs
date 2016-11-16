d23be32833c94d08f40141a6bf66cf41619e1756
shadsluiter <shadsluiter@gmail.com>
2016-11-16 15:16:51 -0700

    Add files via upload

---
+++ madlibs-2.js
@@ -0,0 +1,43 @@
+$('document').ready( function() {
+
+	$('#btnGo').click( function() {
+
+		var x = $('#txtName').val();
+		$('.name').html(x);
+
+		x = $('#txtRestaurant').val();
+		$('#restaurant').html(x);
+
+		x = $('#txtNumber').val();
+		$('#number').html(x);
+
+		x = $('#txtCompanion').val();
+		$('#companion').html(x);
+
+		x = $('#txtAdjective').val();
+		$('#adjective').html(x);
+		
+		x = $('#txtPeopleAtCircus').val();
+		$('#peopleAtCircus').html(x);
+	
+		x = $('#txtMusicStyle').val();
+		$('#musicStyle').html(x);
+	
+		x = $('#xtGame').val();
+		$('#game').html(x);
+	
+		x = $('#txtDesert').val();
+		$('.desert').html(x);
+	
+		x = $('#txtTypeOfSport').val();
+		$('#typeOfSport').html(x);
+	
+		x = $('#txtKindOfEmployee').val();
+		$('#kindOfEmployee').html(x);
+
+		$('#theQuestions').hide();
+		$('#theStory').show();
+	});
+
+
+});
